0dac9aa59792b99702404bda0e463f973cd60c8f
suhilmi01 <suhilmi01@mail.com>
2026-03-31 16:48:42 +0700

    input

--- looping3.cpp
+++ looping3.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;

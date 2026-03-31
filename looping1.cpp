e7175b5d558ccd7fe2fd5bd1acb6b3f64c1dffdf
suhilmi01 <suhilmi01@mail.com>
2026-03-31 16:20:30 +0700

    pengulangan FOR

--- looping1.cpp
+++ looping1.cpp
@@ -2,3 +2,10 @@
 #include <ctime>
 using namespace std;
 
+int main(){
+    int x;
+    cout<<"PERULANGAN FOR"<<endl;
+    for(int i=0;i<=4; i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
+    cout<<endl;

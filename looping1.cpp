02930ee7cd1032d25e8482f2283bbe0fc982e748
suhilmi01 <suhilmi01@mail.com>
2026-03-31 16:29:24 +0700

    perulangan dengan while

--- looping1.cpp
+++ looping1.cpp
@@ -9,3 +9,13 @@ int main(){
         cout<<"Teknologi Informasi UMY"<<endl;
     }
     cout<<endl;
+    srand(time(0));
+    
+    cout<<"PERULANGANN WHILE"<<endl;
+    x = 1 + rand() % 10;
+    while(x <= 5){
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand() % 10;
+    }
+    cout<<"Bilangan acak while yang terakhir ="<<x <<endl;
+    cout<<endl;

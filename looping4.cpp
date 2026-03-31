c4896673b12499db3bea0c126d51e47a4e2364ad
suhilmi01 <suhilmi01@mail.com>
2026-03-31 16:54:58 +0700

    operator logika end

--- looping4.cpp
+++ looping4.cpp
@@ -8,3 +8,12 @@ int main(){
     cin>>nilM;
     cout<<"Masukkan nilai Bahasa Inggris = ";
     cin>>nilB;
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) && (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+}

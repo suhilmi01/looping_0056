cfdc221108194e8e4c3ee8f2799b392d463e0291
suhilmi01 <suhilmi01@mail.com>
2026-03-31 16:39:34 +0700

    variabel

--- looping2.cpp
+++ looping2.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
+int main(){
+    float nilB, nilM, rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
